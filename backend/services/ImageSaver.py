87579dfbc2b4caee217788ea7263927fc82eb653
rhen-ren <zerotwo.alanofren@outlook.com>
2026-04-08 17:50:08 +0800

    feat: add ImageSaver service for saving images to the filesystem

---
+++ backend/services/ImageSaver.py
@@ -0,0 +1,14 @@
+import os
+class ImageSaver():
+    folder = "backend/ProductImages"
+
+    @staticmethod
+    def ensureFolder():
+        os.makedirs(ImageSaver.folder, exist_ok=True)
+
+    @staticmethod
+    def saveImage(imageName:str, image:bytes):
+        filePath = os.path.join(ImageSaver.folder, imageName)
+        with open(filePath, "wb") as f:
+            f.write(image)
+        return filePath
