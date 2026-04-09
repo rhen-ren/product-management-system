26ce0c4d749b745c6e944360f670554cefc98a6b
rhen-ren <zerotwo.alanofren@outlook.com>
2026-04-09 20:28:53 +0800

    fix: normalize file path in saveImage method

--- backend/services/ImageSaver.py
+++ backend/services/ImageSaver.py
@@ -9,6 +9,7 @@ class ImageSaver():
     @staticmethod
     def saveImage(imageName:str, image:bytes):
         filePath = os.path.join(ImageSaver.folder, imageName)
+        filePath = os.path.normpath(filePath)
         with open(filePath, "wb") as f:
             f.write(image)
         return filePath
