d357f0f552568b86386d5e7b684eda181e1bceb9
rhen-ren <zerotwo.alanofren@outlook.com>
2026-04-11 18:29:53 +0800

    refactor: clean up formatting and remove unnecessary comments in db.py

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,18 +1,15 @@
 import mysql.connector
 
-#connect to db
 
-
+# connect to db
 def get_db():
     db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="@zer02woKun",
-    database="ecommerce_db")
+        host="localhost",
+        user="root",
+        password="@zer02woKun",
+        database="ecommerce_db")
 
     try:
         yield db
     finally:
         db.close()
-
-    
