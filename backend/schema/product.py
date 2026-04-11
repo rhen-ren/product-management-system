75f8b3e6424192f0224b53e73206f647d18932c1
rhen-ren <zerotwo.alanofren@outlook.com>
2026-04-11 18:32:10 +0800

    feat: update ProductBase schema to use float for price and stock, and add ProductResponse class for image URL

--- backend/schema/product.py
+++ backend/schema/product.py
@@ -1,10 +1,15 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
 
 class ProductBase(BaseModel):
-    product_id: int = Field(min_length=1)
+    product_id: int = Field(ge=0)
     name: str = Field(max_length=25)
-    price: int = Field(ge=0)
-    stock: int = Field(ge=0)
+    price: float = Field(ge=0)
+    stock: float = Field(ge=0)
     category_id: int = Field(gt=0)
-    category_name: str = Field(min_length=1)
+
+
+class ProductResponse(ProductBase):
+    image_url: Optional[str] = None
