c832eb40678ab18fc6181d6025d1f011a990b7e8
rhen-ren <zerotwo.alanofren@outlook.com>
2026-04-11 18:30:10 +0800

    refactor: improve code structure and update update_product endpoint to use ProductBase schema

--- backend/controllers/productController.py
+++ backend/controllers/productController.py
@@ -1,26 +1,32 @@
-import json
-
-from fastapi import APIRouter, File, Form, Request, Depends, UploadFile
+from typing import Optional
+from fastapi import APIRouter, File, Depends, UploadFile, Path
 from fastapi.responses import JSONResponse
+from schema.product import ProductBase
 from services import product_service
 from database.db import get_db
+from dependency.get_product_from_form import product_from_form
 
 
 router = APIRouter()
 
+
 @router.get("/products", response_class=JSONResponse)
-def get_products(db = Depends(get_db)):
+def get_products(db=Depends(get_db)):
     return product_service.get_products(db)
 
-@router.get("/products/{product_id}", response_class= JSONResponse)
-def get_product(product_id, db = Depends(get_db)):
+
+@router.get("/products/{product_id}", response_class=JSONResponse)
+def get_product(product_id, db=Depends(get_db)):
     return product_service.get_product(product_id, db)
 
-@router.post("/products", response_class= JSONResponse)
-def create_product(product: dict, db = Depends(get_db)):
+
+@router.post("/products", response_class=JSONResponse)
+def create_product(product: dict, db=Depends(get_db)):
     return product_service.create_product(product, db)
 
-@router.put("/products/{product_id}", response_class=JSONResponse)
-def update_product(product_id, image: UploadFile = File(), product: str = Form(), db = Depends(get_db)):
-    product = json.loads(product)
-    return product_service.update_product(product_id, product, image , db)
+
+@router.put("/products/{product_id_path}", response_class=JSONResponse)
+def update_product(product_id_path: int = Path(...), image: Optional[UploadFile] = File(None),
+                   product: ProductBase = Depends(product_from_form),
+                   db=Depends(get_db)):
+    return product_service.update_product(product_id_path, product, image, db)
